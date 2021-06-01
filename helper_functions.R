aa3a6ed74efa1e3d705e5b06ff7b6e226eb0d595
chichaumiau <chichaumiau@gmail.com>
2021-06-01 17:36:57 +0100

    Chichau 1Jun2021

--- helper_functions.R
+++ helper_functions.R
@@ -964,6 +964,9 @@ print('----a')
 }
 
 ########### New Functions ###############################
+#############################
+# Conversion
+
 read_h5<-function(h5file){
     suppressPackageStartupMessages(library(reticulate))
     suppressPackageStartupMessages(library(Matrix))
@@ -1047,29 +1050,9 @@ anndata2expressionset<-function(h5file, save){
     ad = Biobase::ExpressionSet(assayData = as.matrix(df$X),phenoData = Biobase::AnnotatedDataFrame(df$obs))
     saveRDS(ad, save)
 }
-
-library(ggplot2)
-library(DeconRNASeq)                    
-plotTopResults<-function(x, n=9, ncols=3){
-    parray <- ggplot()
-    length(parray) <- n
-    i = 1
-    for(ts in head(unique(x$tissue), n=n)){
-        y = x[x$tissue==ts,]
-    parray[[i]]<-ggplot(y, aes(x=observed_values, y=expected_values)) + geom_point(alpha=.6, color="blue")+
-      geom_abline(intercept=0, slope=1, colour = "red", size = 1)
-        i=i+1
-    }
-    g<-DeconRNASeq::multiplot(plotlist = parray, cols=ncols)
-    return(g)
-}
-                        
-plotAllResults<-function(x, n=9, ncols=3, title=''){
-    p<-ggplot(x, aes(x=observed_values, y=expected_values)) + geom_point(color='blue', alpha=0.4)+
-  geom_smooth(method=lm, color='red')+ggtitle(title)
-    return(p)
-}
-                        
+            
+###########################
+# Evaluation            
 evaluation_metrics<-function(RESULTS, mode=1){
     # return the evaluation metrics
     
@@ -1116,4 +1099,166 @@ getname<-function(params){
         n = paste0(n, j, sep = ".")
     }
     return(n)
+}
+
+########################
+# Plot
+colors = c('#1f77b4',
+ '#ff7f0e',
+ '#279e68',
+ '#d62728',
+ '#aa40fc',
+ '#8c564b',
+ '#e377c2',
+ '#b5bd61',
+ '#17becf',
+ '#aec7e8',
+ '#ffbb78',
+ '#98df8a',
+ '#ff9896',
+ '#c5b0d5',
+ '#c49c94',
+ '#f7b6d2',
+ '#dbdb8d',
+ '#9edae5',
+ '#ad494a',
+ '#8c6d31',
+ '#FFFF00',
+ '#1CE6FF',
+ '#FF34FF',
+ '#FF4A46',
+ '#008941',
+ '#006FA6',
+ '#A30059',
+ '#FFDBE5',
+ '#7A4900',
+ '#0000A6',
+ '#63FFAC',
+ '#B79762',
+ '#004D43',
+ '#8FB0FF',
+ '#997D87',
+ '#5A0007',
+ '#809693',
+ '#6A3A4C',
+ '#1B4400',
+ '#4FC601',
+ '#3B5DFF',
+ '#4A3B53',
+ '#FF2F80',
+ '#61615A',
+ '#BA0900',
+ '#6B7900',
+ '#00C2A0',
+ '#FFAA92',
+ '#FF90C9',
+ '#B903AA',
+ '#D16100',
+ '#DDEFFF',
+ '#000035',
+ '#7B4F4B',
+ '#A1C299',
+ '#300018',
+ '#0AA6D8',
+ '#013349',
+ '#00846F',
+ '#372101',
+ '#FFB500',
+ '#C2FFED',
+ '#A079BF',
+ '#CC0744',
+ '#C0B9B2',
+ '#C2FF99',
+ '#001E09',
+ '#00489C',
+ '#6F0062',
+ '#0CBD66',
+ '#EEC3FF',
+ '#456D75',
+ '#B77B68',
+ '#7A87A1',
+ '#788D66',
+ '#885578',
+ '#FAD09F',
+ '#FF8A9A',
+ '#D157A0',
+ '#BEC459',
+ '#456648',
+ '#0086ED',
+ '#886F4C',
+ '#34362D',
+ '#B4A8BD',
+ '#00A6AA',
+ '#452C2C',
+ '#636375',
+ '#A3C8C9',
+ '#FF913F',
+ '#938A81',
+ '#575329',
+ '#00FECF',
+ '#B05B6F',
+ '#8CD0FF',
+ '#3B9700',
+ '#04F757',
+ '#C8A1A1',
+ '#1E6E00',
+ '#7900D7',
+ '#A77500',
+ '#6367A9',
+ '#A05837',
+ '#6B002C',
+ '#772600',
+ '#D790FF',
+ '#9B9700',
+ '#549E79',
+ '#FFF69F',
+ '#201625',
+ '#72418F',
+ '#BC23FF',
+ '#99ADC0',
+ '#3A2465',
+ '#922329',
+ '#5B4534',
+ '#FDE8DC',
+ '#404E55',
+ '#0089A3',
+ '#CB7E98',
+ '#A4E804',
+ '#324E72')
+library(ggplot2)
+library(DeconRNASeq)                    
+plotTopResults<-function(x, n=9, ncols=3){
+    parray <- ggplot()
+    length(parray) <- n
+    i = 1
+    for(ts in head(unique(x$tissue), n=n)){
+        y = x[x$tissue==ts,]
+    parray[[i]]<-ggplot(y, aes(x=observed_values, y=expected_values, color=CT)) + 
+        geom_point(alpha=.7)+
+        geom_abline(intercept=0, slope=1, colour = "red", size = 1)+ 
+        theme(panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(), 
+              axis.line = element_line(colour = "black"),
+#               text = element_text(size = 20),
+              legend.position = "none")+
+        scale_color_manual(values=colors)
+        i=i+1
+    }
+    g<-DeconRNASeq::multiplot(plotlist = parray, cols=ncols)
+    return(g)
+}
+plotAllResults<-function(x, n=9, ncols=3, title=''){
+    p<-ggplot(x, aes(x=observed_values, y=expected_values, color=CT)) + 
+        geom_point(alpha=.7)+
+        geom_smooth(method=lm, color='red')+
+        ggtitle(title)+ 
+        theme(panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(), 
+              axis.line = element_line(colour = "black"),
+              text = element_text(size = 20),
+              legend.key = element_rect(colour = "transparent", fill = "white"))+
+        scale_color_manual(values=colors)
+    return(p)
 }
